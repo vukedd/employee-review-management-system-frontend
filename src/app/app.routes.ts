a2bc635d123a4726caeb772e270aca0a865c524e
vukedd <vukedd@gmail.com>
2025-07-04 23:57:32 +0200

    Implemented auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './layout/dashboard/dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent,
         title: 'Dashboard',
-        canActivate: []
+        canActivate: [AuthGuard]
     },
 ];
